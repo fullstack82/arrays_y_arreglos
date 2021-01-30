5fd54d58d5b58623f5113dab227b271f435017d6
fullstack82 <fullstack8210@gmail.com>
2021-01-30 19:55:51 +0100

    explicación de que es un array

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/* 
+    ARRAYS : Un array es un conjunto de variables de una misma clase. Se accede a cada elemento individual del array mediante un número 
+* entero denominado indice. 0 es el indice o localizador del primer elemento y es n-1 es el indice del último elemento, siendo N la dimensión,
+* tamaño o longitud del array. Así $empleado[123] representaria al empleado con un localizador 23 como veremos a continuación;
+* 
+* Los arrays son muy utilizados en la programación. Dependiendo de la cantidad de dimensiones que tengan pueden ser:
+* - De una dimensión (vectores). Por ejemplo $empleado[23];
+* - De dos dimensiones (matrices). Por ejemplo $butaca[3, 14];
+* - De tres o más dimensiones (multidimensionales). POr ejemplo: $fecha[2021,02,01];
+*
+*
+* Para acceder a los elementos del array se utilizan los corchetes[], dentro de los cuales existirá un localizador o indice que es un número
+* entero. Además, podemos guardar valores de cualquier tipo de variable (string, entero, punto flotante, booleano) dentro de un array. Por 
+* ejemplo $empleado[23] podria tomar como valor $empleado[23] = " Maria leonor pinto ". En este caso se trataria de un array de cadenas de texto,
+* es decir, un array de string. En otro caso $empleado[23] podria tomar como valor $empleado[23] = 2312. En este caso se trataria de un array de 
+* valores númericos.
+*/
